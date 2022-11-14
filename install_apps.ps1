bc346637830d2995fd303437bcf25e2ea4a03485
Aaron Ankiel <aaron.ankiel@outlook.com>
2022-11-13 23:48:15 -0500

    updated app install script

--- install_apps.ps1
+++ install_apps.ps1
@@ -20,6 +20,9 @@ $apps=  # drivers
         "intel-bluetooth-drivers",
         "nvidia-display-driver",        
         # tools
+        "powershell-core",
+        "wsl",
+        "wsl-ubuntu-2204", # update?
         "7zip", 
         "googlechrome",
         "hyper",
@@ -31,8 +34,6 @@ $apps=  # drivers
         "samsung-magician",
         "discord",
         "signal",
-        "wsl",
-        "wsl-ubuntu-2204", # update?
         "msiafterburner",
         "bitvise-ssh-client",
         "teamviewer",
@@ -45,13 +46,11 @@ $apps=  # drivers
         "ruby",
         "lua",
         "nodejs", 
-        "powershell-core",
         "go",
         # misc
         "shotcut",
         "spotify",
-        "steam-client",
-
+        "steam-client"
 
 # install each app in array
 Foreach ($app in $apps) {
@@ -60,4 +59,34 @@ Foreach ($app in $apps) {
 
 # install battle.net client
 wget https://www.battle.net/download/getInstallerForGame?os=win&gameProgram=BATTLENET_APP&version=Live
-./Battle.net-Setup.exe
+./Battle.net-Setup.exe --lang=enUS --installpath="D:\Program Files (x86)\Battle.net"
+
+## Uninstall BNET
+#
+# function Stop-ProcessName {
+#     param ($ExeName)
+    
+#     while ($true) {
+#         [array]$Processes = Get-Process $ExeName -ErrorAction SilentlyContinue
+#         if($Processes.Length -gt 0) {
+#             Foreach($process in $Processes) {
+#             Stop-Process -Name $process -Force
+#         }
+#         }
+#
+# }
+# }
+#
+# Stop-ProcessName('Agent')
+# Stop-ProcessName('Battle.net')
+#
+# # app files
+# Remove-Item -Recurse /S "$env:LocalAppData\Battle.net" -Force
+# Remove-Item -Recurse /S "$env:LocalAppData\Blizzard Entertainment" -Force
+# Remove-Item -Recurse /S "$env:AppData\Battle.net" -Force
+# Remove-Item -Recurse /S "$env:ProgramData\Battle.net" -Force
+# Remove-Item -Recurse /S "$env:ProgramFiles(x86)\Battle.net" -Force
+# Remove-Item -Recurse /S "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\Battle.net*" -Force
+# Remove-Item -Recurse /S "$env:PUBLICDesktop\Battle.net.lnk" -Force
+# # registry
+# Remove-Item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Battle.net -Force
